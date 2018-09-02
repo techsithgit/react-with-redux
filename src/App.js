b669c1c632646253a8391a7727ce0f8da791d95a
techsithgit <hemilp@tuplecorp.com>
2018-09-02 13:02:30 -0700

    added styles

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-
-//its a function that returns higer order compoent
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <div>your age: <span>{this.props.age}</span></div>
-         <button onClick={this.props.onAgeUp}>Age UP</button>
-         <button onClick={this.props.onAgeDown}>Age Down</button>
+        <div className="Age-label">
+          your age: <span>{this.props.age}</span>
+        </div>
+        <button onClick={this.props.onAgeUp}>Age UP</button>
+        <button onClick={this.props.onAgeDown}>Age Down</button>
       </div>
     );
   }
 }
 
 const mapStateToProps = state => {
- return {
+  return {
     age: state.age
   };
 };
 
 const mapDispachToProps = dispatch => {
   return {
-    onAgeUp: () => dispatch({type:'AGE_UP', value:1}),
-    onAgeDown: () => dispatch({type:'AGE_DOWN', value:1}),
+    onAgeUp: () => dispatch({ type: "AGE_UP", value: 1 }),
+    onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 1 })
   };
 };
-export default connect(mapStateToProps, mapDispachToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispachToProps
+)(App);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,21 +5,13 @@ const initialState = {
 const reducer = (state=initialState, action) => {
     const newState = {...state};
 
-    // if(action.type === 'AGE_UP'){
-    //      newState.age += action.value;
-    // }
-
-    //  if(action.type === 'AGE_DOWN'){
-    //     newState.age -= action.value;
-    //  }
-
     switch(action.type){
         case 'AGE_UP': 
             newState.age += action.value;
             break;
         
         case 'AGE_DOWN': 
-            newState.age += action.value;
+            newState.age -= action.value;
             break;
     }
     return newState;
